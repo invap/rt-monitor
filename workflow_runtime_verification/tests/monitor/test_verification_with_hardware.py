01b413906c667d682af39514e41ab68b6205b6af
Julian Arnesino <jarnesino@eryx.co>
2023-12-26 18:43:41 -0300

    Test hardware event reporting happy path

---
+++ workflow_runtime_verification/tests/monitor/test_verification_with_hardware.py
@@ -0,0 +1,38 @@
+import wx
+
+from workflow_runtime_verification.hardware_devices.c_example_app.ex_adc import adc
+from workflow_runtime_verification.monitor import Monitor
+from workflow_runtime_verification.tests.test import Test
+
+
+class VerificationWithHardwareTest(Test):
+    def test_verifies_a_valid_report_verifying_hardware_properties(self):
+        app = wx.App()  # Remove
+        workflow_specification = self.objects.workflow_specification_with_one_task()
+        hardware_dictionary = {
+            "adc": self._hardware_component()
+        }
+        monitor = Monitor(
+            workflow_specification,
+            hardware_dictionary,
+        )
+        event_report = [
+            self.objects.hardware_encoded_event(
+                self._hardware_device(),
+                self._hardware_data()
+            ),
+        ]
+
+        is_report_valid = monitor.run(event_report)
+
+        self.assertTrue(is_report_valid)
+
+    def _hardware_component(self):
+        return adc()
+
+    def _hardware_device(self):
+        return "adc"
+
+    def _hardware_data(self):
+        return "sample,2042"
+
--- workflow_runtime_verification/tests/test_object_factories/test_encoded_event_factory.py
+++ workflow_runtime_verification/tests/test_object_factories/test_encoded_event_factory.py
@@ -4,6 +4,7 @@ from workflow_runtime_verification.reporting.event.checkpoint_reached_event impo
 from workflow_runtime_verification.reporting.event.declare_variable_event import (
     DeclareVariableEvent,
 )
+from workflow_runtime_verification.reporting.event.hardware_event import HardwareEvent
 from workflow_runtime_verification.reporting.event.task_finished_event import (
     TaskFinishedEvent,
 )
@@ -69,6 +70,13 @@ class TestEncodedEventFactory(TestNameAndValueFactory):
             f"(no_value_delimiter@{self.time()})"
         )
 
+    def hardware_encoded_event(self, hardware_device, hardware_data):
+        return self.event_reporter().report_hardware_event(
+            hardware_device,
+            hardware_data,
+            self.time(),
+        )
+
     def report_without_task_events(self):
         return [
             self.declared_variable_encoded_event(
@@ -107,10 +115,15 @@ class EventReporter:
     def report_declared_variable(self, variable_name, variable_type, time):
         return DeclareVariableEvent(variable_name, variable_type, time).serialized()
 
+    def report_checkpoint_reached(self, checkpoint_name, time):
+        return CheckpointReachedEvent(checkpoint_name, time).serialized()
+
     def report_variable_value_assigned(self, variable_name, variable_value, time):
         return VariableValueAssignedEvent(
             variable_name, variable_value, time
         ).serialized()
 
-    def report_checkpoint_reached(self, checkpoint_name, time):
-        return CheckpointReachedEvent(checkpoint_name, time).serialized()
+    def report_hardware_event(self, hardware_device, hardware_data, time):
+        return HardwareEvent(
+            hardware_device + "," + hardware_data, time
+        ).serialized()
