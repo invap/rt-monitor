b11ab35b6414eb9e7a759d55c611b870d330d554
Julian Arnesino <jarnesino@eryx.co>
2023-12-29 12:11:00 -0300

    Refactor ADC visual component

--- workflow_runtime_verification/hardware_devices/c_example_app/ex_adcVisual.py
+++ workflow_runtime_verification/hardware_devices/c_example_app/ex_adcVisual.py
@@ -8,29 +8,32 @@ class adcVisual(wx.Frame):
         )
 
         self.adc_component = adc_component
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.status_text = wx.TextCtrl(self, -1, "", size=(600, -1))
-        self.sizer = wx.BoxSizer(wx.HORIZONTAL)
-        self.sizer.Add(self.status_text)
-        self.Centre()
-        self.status_text.SetLabel("Iniciando...")
-        # Init timer to reload the data from Logic
+        self._set_up_status_information_components()
+
         self.timer = wx.CallLater(50, self.on_timer)
 
-        # show
+        self.Centre()
         self.Show()
+
+    def _set_up_status_information_components(self):
+        self.counter = wx.TextCtrl(self, -1, "", size=(600, -1))
+        self.counter.SetLabel("Iniciando...")
+        self.sizer.Add(self.counter)
 
     def close(self):
         self.timer.Stop()
         self.Close(True)
 
     def on_timer(self):
-        # update values
-        self.status_text.SetValue(
-            f"Cant. Total de lecturas: {self.adc_component.get_status()[0]}\n"
+        self.counter.SetValue(
+            f"Cantidad de lecturas: {self.adc_component.get_status()[0]}\n"
             + f"Valor actual: {self.adc_component.get_status()[1]} <<== [{str(bin(self.adc_component.get_status()[1]))[2:]}]\n"
         )
-        self.status_text.Refresh()
+        self.counter.Refresh()
+
         self.Refresh()
         self.Update()
+
         self.timer.Restart(50)
