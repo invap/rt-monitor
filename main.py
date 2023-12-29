f0bc65ac50da8203239e1057d2100f72e02b389b
Julian Arnesino <jarnesino@eryx.co>
2023-12-29 17:10:01 -0300

    Improve disabling of folder selection text fields

--- main.py
+++ main.py
@@ -214,8 +214,7 @@ class SimulationPanel(wx.Panel):
     def _set_up_log_file_selection_components(self):
         action_label = "Seleccionar archivo de reporte de eventos:"
         action = self.select_report
-        self.event_report_file_path_field = wx.TextCtrl(self, -1, "", size=(600, 33))
-        self.event_report_file_path_field.Disable()
+        self.event_report_file_path_field = wx.TextCtrl(self, -1, "", size=(600, 33), style=wx.TE_READONLY)
 
         self._set_up_file_selection_components_with(
             action, action_label, self.event_report_file_path_field
@@ -225,9 +224,8 @@ class SimulationPanel(wx.Panel):
         action_label = "Seleccionar archivo de especificación del framework:"
         action = self.select_specification
         self.framework_specification_file_path_field = wx.TextCtrl(
-            self, -1, "", size=(600, 33)
+            self, -1, "", size=(600, 33), style=wx.TE_READONLY
         )
-        self.framework_specification_file_path_field.Disable()
 
         self._set_up_file_selection_components_with(
             action, action_label, self.framework_specification_file_path_field
