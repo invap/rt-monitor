55e7ca8a5c93cfbc148a1d209507e129c6ac5263
Julian Arnesino <jarnesino@eryx.co>
2023-12-29 16:19:46 -0300

    Improve display in ADC visual component

--- workflow_runtime_verification/hardware_devices/c_example_app/ex_adcVisual.py
+++ workflow_runtime_verification/hardware_devices/c_example_app/ex_adcVisual.py
@@ -45,15 +45,17 @@ class adcVisual(wx.Frame):
     def _set_up_counter_display(self):
         counter_display_sizer = wx.BoxSizer(wx.HORIZONTAL)
 
-        self.counter_display_label = wx.StaticText(self, label="Cantidad de lecturas: ")
-        counter_display_sizer.Add(self.counter_display_label, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL, border=10)
+        counter_display_label = wx.StaticText(self, label="Cantidad de lecturas: ")
+        counter_display_sizer.Add(counter_display_label, 0, wx.ALL | wx.ALIGN_CENTER_VERTICAL | wx.ALIGN_LEFT, border=10)
+
+        counter_display_sizer.AddStretchSpacer()
 
         self.counter_display_number = wx.StaticText(
             self, label=self._counter_value(), style=wx.ALIGN_RIGHT
         )
         self._set_up_display(self.counter_display_number, counter_display_sizer)
 
-        self.sizer.Add(counter_display_sizer, 0, wx.CENTER)
+        self.sizer.Add(counter_display_sizer, 0, wx.EXPAND)
 
     def _set_up_value_display(self):
         value_display_sizer = wx.BoxSizer(wx.HORIZONTAL)
@@ -68,7 +70,7 @@ class adcVisual(wx.Frame):
         )
         self._set_up_display(self.measured_value_display, value_display_sizer, self._blue())
 
-        self.sizer.Add(value_display_sizer, 1, wx.EXPAND)
+        self.sizer.Add(value_display_sizer, 0, wx.EXPAND)
 
     def _set_up_binary_value_display(self):
         self.measured_binary_value_display = wx.StaticText(
