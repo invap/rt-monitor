76b549cdad8b607e92dae97d59f7d749b41daf6f
Julian Arnesino <jarnesino@eryx.co>
2024-03-29 00:11:24 -0300

    Change semantics in tests

--- workflow_runtime_verification/tests/monitor/test_verification_with_components.py
+++ workflow_runtime_verification/tests/monitor/test_verification_with_components.py
@@ -7,17 +7,17 @@ from workflow_runtime_verification.monitor import Monitor
 from workflow_runtime_verification.tests.test import Test
 
 
-class VerificationWithHardwareTest(Test):
-    def test_verifies_a_valid_report_verifying_hardware_properties(self):
+class VerificationWithComponentsTest(Test):
+    def test_verifies_a_valid_report_verifying_component_properties(self):
         _app = wx.App()
         workflow_specification = self.objects.workflow_specification_with_one_task()
-        hardware_dictionary = {
-            self._hardware_component_name(): self._hardware_component()
+        component_dictionary = {
+            self._component_name(): self._component()
         }
-        monitor = Monitor(workflow_specification, hardware_dictionary)
+        monitor = Monitor(workflow_specification, component_dictionary)
         event_report = [
             self.objects.hardware_encoded_event(
-                self._hardware_component_name(), self._hardware_event_data()
+                self._component_name(), self._component_event_data()
             ),
         ]
 
@@ -25,11 +25,11 @@ class VerificationWithHardwareTest(Test):
 
         self.assertTrue(is_report_valid)
 
-    def _hardware_component(self):
+    def _component(self):
         return adc()
 
-    def _hardware_component_name(self):
+    def _component_name(self):
         return "adc"
 
-    def _hardware_event_data(self):
+    def _component_event_data(self):
         return "sample,2042"
